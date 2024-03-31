f7b15faf4186bb818d9b729f10a07114cab28c78
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-31 08:45:38 +0530

    [ADDED]: harshal this proram is Boolean Data type in c#

---
+++ Data_Type_boolean.cs/Data_Type_boolean.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data_Type_boolean.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Valid = true;
+            bool valid = false;
+
+            Console.WriteLine(valid);
+            Console.WriteLine(valid);
+
+            Console.ReadLine();
+
+        }
+    }
+}
