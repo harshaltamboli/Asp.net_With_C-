d74bc382ce846410a4ba4ebea555ec0104023945
harshaltamboli <harshaltamboli429@gmail.com>
2024-04-03 20:57:44 +0530

    [ADDED]: harshal this proram is operator for Arithmatc

---
+++ Arithmatic_operator.cs/Arithmatic_operator.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arithmatic_operator.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1=15, num2=5, result = 0;
+
+            Console.WriteLine("Arithmatic Operater");
+
+            result = num1 + num2;
+            Console.WriteLine("Arithmatic = "+result);
+
+            result = num1 - num2;
+            Console.WriteLine("minus = " + result);
+
+            result = num1 * num2;
+            Console.WriteLine("multiplication = " + result);
+
+            result = num1 % num2;
+            Console.WriteLine("modulas = " + result);
+
+            result = num1 / num2;
+            Console.WriteLine("division = " + result);
+
+
+            Console.ReadLine();
+        }
+    }
+}
